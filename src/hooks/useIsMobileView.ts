c48cd75ce683bce83eaf50009e9a82b9798f2283
Ethan Standel <ethan.standel@rocketinsights.com>
2021-10-10 04:05:11 -0400

    fix(client): soothe SSR hydration errors caused by immediately returning hook

--- src/hooks/useIsMobileView.ts
+++ src/hooks/useIsMobileView.ts
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+
 import { useMediaQuery } from "@chakra-ui/media-query";
 
 const useIsMobileView = () => {
-  const [isMobileView] = useMediaQuery("(max-width: 750px)");
+  // we know that the server says false
+  const [isMobileView, setIsMobileView] = useState(false);
+  const [isMobileViewQuery] = useMediaQuery("(max-width: 750px)");
+
+  // This helps the Next SSR/hydration cycle
+  useEffect(() => {
+    setIsMobileView(isMobileViewQuery);
+  }, [isMobileViewQuery]);
+
   return isMobileView;
 };
 
