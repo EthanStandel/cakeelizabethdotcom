c993d82fbeeda4085c6b316cb6775f239b41998e
Ethan Standel <ethan.standel@rocketinsights.com>
2021-10-28 01:14:12 -0400

    feat: adds imageManifest.json generator for all [product] pages

---
+++ scripts/build-image-manifest.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const pagesDir = "./public/resources/pages/";
+
+const pages = fs.readdirSync(pagesDir).filter(name => !name.startsWith("."));
+
+pages.forEach(page => {
+  const images = fs.readdirSync(pagesDir + page)
+    .filter(file => [".gif", ".png", ".jpg", ".jpeg", ".svg"].some(suffix => file.endsWith(suffix)))
+    .map(image => `/resources/pages/${page}/${image}`);
+  const manifestExists = fs.existsSync(pagesDir + page + "/imageManifest.json");
+  if (manifestExists) {
+    fs.unlinkSync(pagesDir + page + "/imageManifest.json");
+  }
+  fs.writeFileSync(pagesDir + page + "/imageManifest.json", JSON.stringify(images));
+});
