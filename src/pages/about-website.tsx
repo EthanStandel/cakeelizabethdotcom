4e3c1fdef336e915e96caa8e363ad784ff0ab547
Ethan Standel <ethan.standel@rocketinsights.com>
2021-11-07 11:24:57 -0500

    feat: about-website page and licenses

--- src/pages/about-website.tsx
+++ src/pages/about-website.tsx
@@ -1,17 +1,46 @@
 import { GetStaticProps } from "next";
+import sanitizeHtml from "sanitize-html";
 
 import type content from "../../public/resources/pages/about-website/content.json";
 import staticContentClient from "../clients/staticContentClient";
+import MdRenderer from "../components/MdRenderer";
+import appClasses from "../styles/pages/app.module.sass";
 
-type Content = typeof content;
+type Content = typeof content & { licenses: string; bodyContent: string };
 
-const Page = () => <></>;
+const Page = ({ licenses, bodyContent }: Content) => (
+  <div className={appClasses.pageContainer}>
+    <div className={appClasses.contentContainer}>
+      <MdRenderer input={bodyContent} />
+      <div
+        className={appClasses.htmlRoot}
+        dangerouslySetInnerHTML={{ __html: licenses }}
+      />
+    </div>
+  </div>
+);
 
 export const getStaticProps: GetStaticProps = async () => {
   const props = await staticContentClient.getContentForPage<Content>(
     "about-website"
   );
-  return { props };
+
+  // sanitze because this comes from a generator package and comes with bleh styles
+  const bodyContent = await staticContentClient.getContentForPage<string>(
+    "about-website",
+    "body-content.md",
+    "text"
+  );
+
+  // sanitze because this comes from a generator package and comes with bleh styles
+  const licenses = sanitizeHtml(
+    await staticContentClient.getContentForPage<string>(
+      "about-website",
+      "licenses.html",
+      "text"
+    )
+  );
+  return { props: { ...props, licenses, bodyContent } };
 };
 
 export default Page;
