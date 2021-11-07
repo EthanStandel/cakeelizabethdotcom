1d8871a895040d503837f0b2196758091665eafe
Ethan Standel <ethan.standel@rocketinsights.com>
2021-11-07 18:05:53 -0500

    feat: minor wife-approved menu and icon changes

--- src/hooks/useIsMobileView.ts
+++ src/hooks/useIsMobileView.ts
@@ -5,7 +5,7 @@ import { useMediaQuery } from "@chakra-ui/media-query";
 const useIsMobileView = () => {
   // we know that the server says false
   const [isMobileView, setIsMobileView] = useState(false);
-  const [isMobileViewQuery] = useMediaQuery("(max-width: 999px)");
+  const [isMobileViewQuery] = useMediaQuery("(max-width: 1023px)");
 
   // This helps the Next SSR/hydration cycle
   useEffect(() => {
