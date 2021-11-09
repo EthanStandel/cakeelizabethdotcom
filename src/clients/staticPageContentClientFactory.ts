424667a8617dd73f79ded20f616817f0054d9352
Ethan Standel <ethan.standel@rocketinsights.com>
2021-11-09 01:57:26 -0500

    build: swaps axios for fs for static content providers

--- src/clients/staticPageContentClientFactory.ts
+++ src/clients/staticPageContentClientFactory.ts
@@ -1,17 +1,23 @@
-import axios from "axios";
-
-import environment from "../environment";
-
-// Used when the Node-context needs to make requests to "/"
-const client = axios.create({ baseURL: environment.serviceAddress });
+import fs from "fs";
 
 const staticPageContentClientFactory = (pageName: string) => ({
-  getContent: async <T>(file = "content.json"): Promise<T> =>
-    (
-      await client.get(`/resources/pages/${pageName}/${file}`, {
-        responseType: file.endsWith(".json") ? "json" : "text",
-      })
-    ).data,
+  getContent: async <T>(file = "content.json"): Promise<T> => {
+    const content: string = await new Promise((resolve, reject) =>
+      // strictly use fs to fetch all build-time resources
+      fs.readFile(
+        `./public/resources/pages/${pageName}/${file}`,
+        { encoding: "utf-8" },
+        (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(data);
+        }
+      )
+    );
+
+    return file.endsWith(".json") ? JSON.parse(content) : content;
+  },
 });
 
 export default staticPageContentClientFactory;
