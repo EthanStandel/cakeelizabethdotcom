8b7598a59118f5ffe5ea084d3b3aabefd31e1ee5
Ethan Standel <ethan.standel@rocketinsights.com>
2021-11-09 02:25:55 -0500

    fix: cleanup and accesibility

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,38 @@
 import { useEffect } from "react";
 
+import { GetStaticProps } from "next";
 import { useRouter } from "next/dist/client/router";
 
-const Page = () => {
+import type content from "../../public/resources/pages/404/content.json";
+import staticPageContentClientFactory from "../clients/staticPageContentClientFactory";
+import appClasses from "../styles/pages/app.module.sass";
+
+type Content = typeof content;
+
+const Page = (content: Content) => {
   const router = useRouter();
 
   useEffect(() => {
     router.replace("/");
   });
-  return <></>;
+
+  // Adding content just in case someone has JS turned off
+  // Can't use true redirect because SSR is not allowed for 404 component
+  // and getStaticProps can't return redirect
+  return (
+    <div className={appClasses.pageContainer}>
+      <div className={appClasses.contentContainer}>
+        <h2>{content.text}</h2>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const client = staticPageContentClientFactory("404");
+  const content = await client.getContent<Content>();
+
+  return { props: { ...content } };
 };
 
 export default Page;
