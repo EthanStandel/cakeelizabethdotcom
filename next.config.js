00172003caa91c2130cdf3151d59fc91464e7046
Ethan Standel <ethan.standel@rocketinsights.com>
2021-11-23 01:22:52 -0500

    fix: remove preact because it breaks animxyz (issue filed with animxyz)

--- next.config.js
+++ next.config.js
@@ -23,20 +23,11 @@ module.exports = withContentlayer()({
       permanent: true,
     }
   ]),
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.html$/i,
       loader: "raw-loader",
     });
-
-    // replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
-    }
 
     return config;
   },
