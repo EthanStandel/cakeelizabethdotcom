1afeb666fcdd72400e2dec6e08cd641534f220ec
Ethan Standel <ethan@standel.io>
2022-10-26 00:02:00 -0400

    fix: adds 500 page which fixes build for some reason

---
+++ src/pages/500.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
+
+import styleUtils from "../utils/styleUtils";
+
+import { allPageContents } from ".contentlayer/generated";
+import type { PageContent } from ".contentlayer/generated/types";
+
+type Props = { content: PageContent };
+
+const Page: NextPage<Props> = ({ content }) => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    router.replace("/");
+  });
+
+  // Adding content just in case someone has JS turned off
+  // Can't use true redirect because SSR is not allowed for 404 component
+  // and getStaticProps can't return redirect
+  return (
+    <styleUtils.PageContainer>
+      <styleUtils.ContentContainer>
+        <h2>{content.data.text}</h2>
+      </styleUtils.ContentContainer>
+    </styleUtils.PageContainer>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const content = allPageContents.find(({ page }) => page === "500")!;
+
+  return { props: { content } };
+};
+
+export default Page;
