d0d3d60d1b8d8b132ec41636cf231a5dad7e59e7
Ethan Standel <ethan@standel.io>
2023-03-03 06:15:27 -0500

    feat: better home page editing by combining into one query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,11 @@ import client from "../../.tina/__generated__/client";
 import { HomePageClient } from "./components/Home/HomePageClient";
 
 const Page = async () => {
-  const { data, query, variables } = await client.queries.homePage({
+  const { data, query, variables } = await client.queries.homePageData({
     relativePath: "home-page.md",
   });
-  const {
-    data: productData,
-    query: productQuery,
-    variables: productVariables,
-  } = await client.queries.homePageProducts();
 
-  return (
-    <HomePageClient
-      homePageQuery={{ data, query, variables }}
-      productQuery={{
-        data: productData,
-        query: productQuery,
-        variables: productVariables,
-      }}
-    />
-  );
+  return <HomePageClient homePageQuery={{ data, query, variables }} />;
 };
 
 export default Page;
