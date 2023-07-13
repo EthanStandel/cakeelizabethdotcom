a54c6fd1a3679089aadd7bdcb6566f634a04464f
Ethan Standel <ethan@standel.io>
2023-07-13 04:48:07 -0400

    feat: fix available indexed branch, I think

--- .tina/config.tsx
+++ .tina/config.tsx
@@ -13,7 +13,7 @@ const config = defineConfig({
   branch:
     process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD!, // Netlify branch env
+    "feat/tina-rewrite", // initial default value
   token: process.env.TINA_TOKEN!,
   media: {
     // If you wanted cloudinary do this
