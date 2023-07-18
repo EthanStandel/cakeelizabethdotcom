6577cb90f27c78810f189b878f80d9a553de4784
Ethan Standel <ethan@standel.io>
2023-07-18 12:35:54 -0400

    fix: add autogenerated GQL files to repo

---
+++ .tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/ec7dd7a6-6248-4faa-9e12-a8333fd8a7f7/github/feat/tina-rewrite', token: '2a00cba056e51a5328a05967a04143df668e2d50', queries });
+export default client;
+  
