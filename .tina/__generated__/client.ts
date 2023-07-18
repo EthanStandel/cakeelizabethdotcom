bac5799a80cc101f153ee5795895e7d0311483fe
Ethan Standel <ethan@standel.io>
2023-07-18 13:03:57 -0400

    fix: whoops, time to generate new keys

--- .tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '2a00cba056e51a5328a05967a04143df668e2d50', queries });
-export default client;
-  
